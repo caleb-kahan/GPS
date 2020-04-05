2f510ddef5c65a33695c3e6f523f91d7ec0dfb16
Caleb Kahan <ckahan00@stuy.edu>
2020-04-05 16:03:33 -0400

    drawed images to screen

--- parser.py
+++ parser.py
@@ -83,6 +83,7 @@ def parse_file( fname, csystems, screen, color ):
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step_3d)
             matrix_mult(csystems[-1],polygons)
+            draw_polygons(polygons,screen,color)
 
         elif line == 'torus':
             #print 'TORUS\t' + str(args)
@@ -91,6 +92,7 @@ def parse_file( fname, csystems, screen, color ):
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), step_3d)
             matrix_mult(csystems[-1],polygons)
+            draw_polygons(polygons,screen,color)
 
         elif line == 'box':
             #print 'BOX\t' + str(args)
@@ -99,6 +101,7 @@ def parse_file( fname, csystems, screen, color ):
                     float(args[0]), float(args[1]), float(args[2]),
                     float(args[3]), float(args[4]), float(args[5]))
             matrix_mult(csystems[-1],polygons)
+            draw_polygons(polygons,screen,color)
 
         elif line == 'circle':
             #print 'CIRCLE\t' + str(args)
@@ -107,6 +110,7 @@ def parse_file( fname, csystems, screen, color ):
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step)
             matrix_mult(csystems[-1],edges)
+            draw_polygons(edges,screen,color)
 
         elif line == 'hermite' or line == 'bezier':
             #print 'curve\t' + line + ": " + str(args)
@@ -118,6 +122,7 @@ def parse_file( fname, csystems, screen, color ):
                       float(args[6]), float(args[7]),
                       step, line)
             matrix_mult(csystems[-1],edges)
+            draw_polygons(edges,screen,color)
 
         elif line == 'line':
             #print 'LINE\t' + str(args)
@@ -126,6 +131,7 @@ def parse_file( fname, csystems, screen, color ):
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
             matrix_mult(csystems[-1],edges)
+            draw_polygons(edges,screen,color)
 
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
